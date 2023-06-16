2ba6d6a9d2d686979e9d8e0d5034e207eddb3d23
hakoo <hakoo.programming@gmail.com>
2023-06-16 08:26:46 -0700

    Assigment No.1 for GitHub

---
+++ Q2.dart
@@ -0,0 +1,12 @@
+void main() {
+  int hamza = 24;
+  int munir = 22;
+  if (hamza>munir)
+  {
+    print ("Hamza Older Than Munir");
+    }
+  else {
+    print ("Hamza Younger Than Munir");
+    }
+
+}
